e3d3cdc96b2666b503ab8a57b34536c2605b4a47
Thouhid98 <thouhidhussain98@gmail.com>
2023-11-13 00:56:13 +0600

    Our Menu with dynamic data load done

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,6 +1,10 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../Shared/Cover/Cover';
 import manuImg from '../../assets/menu/banner3.jpg'
+import dessertImg from '../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../assets/menu/pizza-bg.jpg'
+import saladImg from '../../assets/menu/salad-bg.jpg'
+import soupImg from '../../assets/menu/soup-bg.jpg'
 import useMenu from '../../hooks/useMenu';
 import Sectiontitle from '../../components/Sectiontitle/Sectiontitle';
 import MenuCategory from './MenuCategory/MenuCategory';
@@ -9,7 +13,7 @@ import MenuCategory from './MenuCategory/MenuCategory';
 const Menu = () => {
     const [menu] = useMenu();
     const offered = menu.filter(item => item.category === 'offered')
-    const dessert = menu.filter(item => item.category === 'dessert')
+    const desserts = menu.filter(item => item.category === 'dessert')
     const pizza = menu.filter(item => item.category === 'pizza')
     const salad = menu.filter(item => item.category === 'salad')
     const soup = menu.filter(item => item.category === 'soup')
@@ -22,7 +26,18 @@ const Menu = () => {
             <div className='mt-5 mb-2'>
             <Sectiontitle subHeading='---Do not Miss---' heading="Today's Offer"></Sectiontitle>
             </div>
+            {/* main cover  */}
             <MenuCategory items={offered} ></MenuCategory>
+    
+            {/* dessert menu items  */}
+            <MenuCategory items={desserts} title={'Dessert'} img={dessertImg}></MenuCategory>
+            {/* pizz  */}
+            <MenuCategory items={pizza} title={'Pizza'} img={pizzaImg}></MenuCategory>
+            {/* Salad */}
+            <MenuCategory items={salad} title={'Salad'} img={saladImg}></MenuCategory>
+            {/* soup */}
+            <MenuCategory items={soup} title={'Soup'} img={soupImg}></MenuCategory>
+
         </div>
     );
 };
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,8 +1,10 @@
+import Cover from '../../Shared/Cover/Cover';
 import Menuitem from '../../Shared/Menuitem/Menuitem';
 
-const MenuCategory = ({items}) => {
+const MenuCategory = ({items, title, img}) => {
     return (
         <div>
+            {title && <Cover img={img} title={title}></Cover>}
             <div className='grid md:grid-cols-2 gap-8 mt-10 mb-10'>
                 {
                     items.map(item =><Menuitem key={item._id} item={item}></Menuitem>)
